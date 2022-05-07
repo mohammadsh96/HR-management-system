f9cdc9da2344f5513f0336dcf02ca0f29d3911f1
mohammad alshraideh <mhmd.shrydh1996@gmail.com>
2022-05-07 20:09:13 +0300

    fix table and render image

--- accounting.js
+++ accounting.js
@@ -1,38 +1,100 @@
-`use strict`;
+'use strict';
+let allEmployees = [];
+let tableEl = document.getElementById('tableId')
+function Employee(id, name, department, level, image, salary) {
+  this.employeeId = id;
+  this.fullName = name;
+  this.department = department;
+  this.level = level;
+  this.image = image;
+  this.salary = salary;
+  allEmployees.push(this);
+  createTableData(department, Number.parseFloat(salary));
+}
+const tableData = {};
+function createTableData(department, salary) {
+  if (!tableData[department]) {
+    tableData[department] = { employeeCount: 1, salaryTotal: salary, salaryAvg: salary };
+  } else {
+    let employeeCount = tableData[department].employeeCount;
+    let salaryTotal = tableData[department].salaryTotal;
+    tableData[department].employeeCount = employeeCount + 1;
+    tableData[department].salaryTotal = salaryTotal + salary;
+    tableData[department].salaryAvg = tableData[department].salaryTotal / (employeeCount + 1);
 
-let arrayData =[ ];
+  }
+}
 
-function Employee (EmployeeID ,FullName ,Department,Level , imgurl   ){
 
-    this.EmployeeID =EmployeeID ;
-    this.FullName =FullName ;
-    this.Department = Department ;
-    this.Level = Level ; 
-     this.salary = 0 ;
-     arrayData.push(this);
-     
+
+Employee.prototype.tableRender = function () {
+  let allEmployeesCounts = 0;
+  let allEmployeesSalary = 0;
+  for (const [department, values] of Object.entries(tableData)) {
+    let trTd = document.createElement('tr');
+    tableEl.appendChild(trTd);
+
+    let depName = document.createElement('td');
+    depName.textContent = department;
+    trTd.appendChild(depName);
+
+    let numberOfEmplouees = document.createElement('td');
+    numberOfEmplouees.textContent = values.employeeCount;
+    trTd.appendChild(numberOfEmplouees);
+
+    let salaryAvg = document.createElement('td');
+    salaryAvg.textContent = values.salaryTotal;
+    trTd.appendChild(salaryAvg);
+
+    let salaryTotal = document.createElement('td');
+    salaryTotal.textContent = values.salaryAvg;
+    trTd.appendChild(salaryTotal);
+
+    allEmployeesCounts += values.employeeCount;
+    allEmployeesSalary += values.salaryTotal;
+
+  }
+
+  let total = document.createElement('td');
+  total.textContent = 'Total';
+  total.classList = 'total';
+  tableEl.appendChild(total);
+
+  let salaryAllEmployees = document.createElement('td');
+  salaryAllEmployees.textContent = allEmployeesCounts;
+  salaryAllEmployees.classList = 'total';
+  tableEl.appendChild(salaryAllEmployees);
+
+  let salaryAllTotal = document.createElement('td');
+  salaryAllTotal.textContent = allEmployeesSalary;
+  salaryAllTotal.classList = 'total';
+  tableEl.appendChild(salaryAllTotal);
+
+  let salaryAllAvg = document.createElement('td');
+  salaryAllAvg.textContent = allEmployeesSalary / allEmployeesCounts;
+  salaryAllAvg.classList = 'total';
+  tableEl.appendChild(salaryAllAvg);
+};
+
+let employee_1 = new Employee('1000', 'Ghazi Samer', 'Administration', 'Senior', "../assets/Ghazi.jpg", '1700');
+let employee_2 = new Employee('1001', 'Lana Ali', 'Finance', 'Senior', '../assets/Lana.jpg', '1500');
+let employee_3 = new Employee('1002', 'Tamara Ayoub', 'Marketing', 'Senior', '../assets/Tamara.jpg', '1900');
+let employee_4 = new Employee('1003', 'Safi Walid', 'Administration', 'Mid-Senior', '../assets/Safi.jpg', '1300');
+let employee_5 = new Employee('1004', 'Omar Zaid', 'Development', 'Senior', `../assets/Omar.jpg`, '2000');
+let employee_6 = new Employee('1005', 'Rana Saleh', 'Development', 'Junior', '../assets/Rana.jpg', '700');
+let employee_7 = new Employee('1006', 'Hadi Ahmad', 'Finance', 'Mid-Senior', '../assets/Hadi.jpg', '1400');
+
+
+allEmployees[0].tableRender();
+
+function getData() {
+  let retrievedData = localStorage.getItem('employeeItem');
+  let arrayData = JSON.parse(retrievedData);
+  if (arrayData != null) {
+    for (let i = 0; i < arrayData.length; i++) {
+      new Employee(arrayData[i].id, arrayData[i].fullName, arrayData[i].department, arrayData[i].level, arrayData[i].image, arrayData[i].salary);
     }
-
-    let ghazi = new Employee (1000 ,	"Ghazi Samer" ,	 "Administration",	"senior" ,   "imgurl"   );
-    console.table(ghazi) ;
-    
-    let lana = new Employee  (1001,	   "Lana ali" ,     	"Finance",	"senior" ,   "imgurl"   );
-    console.table(lana) ; 
-    
-    let tamara = new Employee (1002 ,	"Tamara Ayoub" ,	 "Marketing", 	"senior" ,     "imgurl"  );
-    console.table(tamara) ; 
-   
-    let safi = new Employee (1003 ,	"Safi Walid" ,	"Administration",	"mid-senior" , "imgurl"   );
-    console.table(safi) ;
-   
-    let omar = new Employee (1004 ,	"Omar Zaid" ,  	"Development",  	"senior" , "imgurl"   );
-    console.table(omar) ; 
-   
-    let rana = new Employee (1005 ,	"Rana Saleh" ,	    "Development",	   "jonior" , "imgurl"  );
-    console.table(rana) ;
-   
-    let hadi = new Employee (1006 ,	"Hadi Ahmad" ,	      "Finance",	"mid-senior" , "imgurl" );
-    console.table(hadi) ; 
-
-
-
+  }
+  allEmployees[0].tableRender();
+}
+getData();
--- app.js
+++ app.js
@@ -3,15 +3,117 @@ let secEl = document.getElementById("secEl");
 let fname = document.getElementById("fname");
 let eventEl = document.getElementById ("formID");
 let image =document.getElementById("img");
-
+allEmployees =[];
 
  eventEl.addEventListener("submit" , handelsubmit ) ;
  function handelsubmit(event){
 event.preventDefault();
 
-console.log(event);
 console.log(event.target.fname.value)
 console.log(event.target.Department.value)
 console.log(event.target.level.value)
 console.log(event.target.img.value)
+let newE = new Employee(this.employeeId, this.FullName, this.department, this.level, this.image, this.salary);
+
+  newE.render();
  }
+ function Employee(id, name, department, level, image, salary) {
+    this.employeeId = id;
+    this.fullName = name;
+    this.department = department;
+    this.level = level;
+    this.image = image;
+    this.salary = salary;
+    allEmployees.push(this);
+  }
+  let ghazi = new Employee (1000 ,	"Ghazi Samer" ,	 "Administration",	"senior" ,   "../assests/Ghazi.jpg"   );
+  console.table(ghazi) ;
+  
+  let lana = new Employee  (1001,	   "Lana ali" ,     	"Finance",	"senior" ,   "../assests/Lana.jpg"   );
+  console.table(lana) ; 
+  
+  let tamara = new Employee (1002 ,	"Tamara Ayoub" ,	 "Marketing", 	"senior" ,     "../assests/Tamara.jpg"  );
+  console.table(tamara) ; 
+ 
+  let safi = new Employee (1003 ,	"Safi Walid" ,	"Administration",	"mid-senior" , "../assests/Safi.jpg"   );
+  console.table(safi) ;
+ 
+  let omar = new Employee (1004 ,	"Omar Zaid" ,  	"Development",  	"senior" , "../assests/Omar.jpg"   );
+  console.table(omar) ; 
+ 
+  let rana = new Employee (1005 ,	"Rana Saleh" ,	    "Development",	   "jonior" , "../assests/Tamara.jpg"  );
+  console.table(rana) ;
+ 
+  let hadi = new Employee (1006 ,	"Hadi Ahmad" ,	      "Finance",	"mid-senior" , "../assests/Omar.jpg" );
+  console.table(hadi) ;
+
+  function getRndIntger(min, max) {
+    return Math.floor(Math.random() *(max - min +1 )) + min;
+  };
+  Employee.prototype.netsalary= function (){
+ 
+    if (this.salary === "senior") {
+      this.salary = getRndIntger(1500 , 2000) ;
+      this.salary = this.salary * (0.925 ) ;
+      document.write ( `<p> employee name : ${this.fullName} & net salary = ${this.salary} </p>`);
+      
+      }
+     
+ 
+       if (this.Level === "mid-senior"){
+     this.salary = getRndIntger(1000 , 1500) ;
+     this.salary = this.salary *(0.925) ;
+     document.write (`<p> employee name  ${this.fullName} & net salary = ${this.salary} </p>`);
+      }
+      if (this.Level === "jonior"){
+     this.salary = getRndIntger(500 , 1000) ;
+     this.salary = this.salary *(0.925) ; 
+     document.write (`<p> employee name  ${this.fullName} & net salary = ${this.salary} </p>`);
+     }
+   }
+   ghazi.netsalary();
+lana.netsalary();
+tamara.netsalary();
+safi.netsalary();
+omar.netsalary();
+rana.netsalary();
+hadi.netsalary();
+ 
+  Employee.prototype.render = function () {
+  let card = document.createElement('div');
+  secEl.appendChild(card);
+  
+  let image = document.createElement('img');
+  image.src = this.image;
+  card.appendChild(image);
+  let name = document.createElement('h5');
+  name.textContent = this.FullName;
+  card.appendChild(name);
+  let par = document.createElement('p');
+  par.textContent = `ID: ${this.employeeId} \n - Department: ${this.department} \n - Level: ${this.level} \n - salary: ${this.salary} JD`;
+  card.appendChild(par);
+}
+
+
+ghazi.render();
+lana.render(); 
+tamara.render();
+safi.render();
+omar.render();
+rana.render();
+hadi.render();
+
+const jsonArr = JSON.stringify(allEmployees);
+
+// save to localStorage
+localStorage.setItem("array", jsonArr);
+
+// get the string
+// from localStorage
+const str = localStorage.getItem("array");
+
+// convert string to valid object
+const parsedArr = JSON.parse(str);
+
+console.log(parsedArr);
+
